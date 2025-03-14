de722064c848da494994274dd03452edd2c3456b
4512yasir <yasiryaska1@gmail.com>
2025-03-14 17:30:59 +0300

    done

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let board = ["", "", "", "", "", "", "", "", ""];    // empty array of the number of tiles
     let currentPlayer = "X";  
     let gameActive = true;
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
